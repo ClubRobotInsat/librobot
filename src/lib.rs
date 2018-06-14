b340e6a95ee57ae708e6a8e3f503a30ff83e6752
Paul Florence <perso@florencepaul.com>
2018-06-14 19:46:16 +0200

    Ajout d'un fichier contraingnant la toolchain utilisée par rustup. Ajout de la compilation conditionnelle pour la librairie standard. Structure de base des trames herkulex.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-//#![no_std]
+#![no_std]
+
 #![warn(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![deny(unsafe_code)]
 #![allow(dead_code)]
 
 //! Librairie du club
+
+// On inclue la librairie standard pour s'en servir dans les tests !
+#[cfg(test)]
+#[macro_use]
+extern crate std;
 
 extern crate arrayvec;
 extern crate embedded_hal;
