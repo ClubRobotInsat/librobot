676eb70cbc3e3eee74b4412744e5703a3c24d696
Paul Florence <perso@florencepaul.com>
2018-11-20 14:34:35 +0100

    Added `from_u8` for MessageKind.

--- src/transmission/mod.rs
+++ src/transmission/mod.rs
@@ -118,3 +118,17 @@ impl Into<u8> for MessageKind {
         }
     }
 }
+
+impl MessageKind {
+    
+    /// Crée un `MessageKind` depuis un entier
+    pub fn from_u8(data : u8) -> Result<MessageKind,()> {
+        match data {
+            4 => Ok(MessageKind::Servo),
+            5 => Ok(MessageKind::Navigation),
+            _ => Err(())
+    
+        }
+    }
+}
+
