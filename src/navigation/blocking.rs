0aeee42d4b8aa59bc64f5844c17c441cb9e03543
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-05-27 18:51:52 +0200

    Add tests for blocking

--- src/navigation/blocking.rs
+++ src/navigation/blocking.rs
@@ -6,6 +6,10 @@ use crate::navigation::motor::Command;
 
 /// Module permettant de detecter si le robot est bloqué. Le robot est
 /// considéré bloqué s'il reçoit une commande non nulle mais ne bouge pas.
+///
+/// Plus précisément, si au moins une roue devrait avancer mais n'avance
+/// pas, le robot est considéré bloqué, sauf si l'autre roue est en train
+/// d'avancer.
 pub struct Blocking {
     command_threshold: u16,
     distance_threshold: f32,
@@ -19,7 +23,7 @@ impl Blocking {
     /// supérieure à cette valeur on considère que le robot reçoit une commande.
     ///
     /// `distance_threshold`: Si la distance parcourue par le robot est inférieure
-    /// à cette valeur on considère que le robot n'a pas changé de position.
+    /// à cette valeur on considère que le robot n'a pas changé de position. En mm.
     pub fn new(command_threshold: u16, distance_threshold: f32) -> Self {
         Blocking {
             command_threshold,
@@ -29,8 +33,17 @@ impl Blocking {
         }
     }
 
+    /// Reset internal tracking data
+    pub fn reset(&mut self) {
+        self.last_dist = (0.0, 0.0);
+        self.blocked = false;
+    }
+
     /// Met à jour l'état de bloquage du robot. Cette fonction doit être appelée
-    /// periodiquement avec une période suffisamment longue (par exemple 100ms)
+    /// periodiquement avec une période suffisamment longue (par exemple 100ms).
+    ///
+    /// `command` La commande actuellement envoyée au moteur
+    /// `dist` La distance totale parcourue par les codeurs, en mm
     pub fn update(&mut self, command: (Command, Command), dist: (f32, f32)) {
         let (left_diff, right_diff) = (dist.0 - self.last_dist.0, dist.1 - self.last_dist.1);
         self.last_dist = dist;
@@ -59,9 +72,46 @@ impl Blocking {
 
 #[cfg(test)]
 mod test {
+    use crate::navigation::blocking::Blocking;
+    use crate::navigation::Command;
 
     #[test]
     fn test_blocking() {
-        // TODO
+        let mut blocking = Blocking::new(100, 0.1);
+
+        // Forward
+        blocking.update((Command::Front(12), Command::Front(12)), (0.05, 0.05));
+        assert!(!blocking.blocked());
+        blocking.reset();
+
+        blocking.update((Command::Front(120), Command::Front(12)), (0.05, 0.05));
+        assert!(blocking.blocked());
+        blocking.reset();
+
+        blocking.update((Command::Front(120), Command::Front(120)), (14.0, 0.05));
+        assert!(!blocking.blocked());
+        blocking.reset();
+
+        // Backward
+        blocking.update((Command::Back(12), Command::Back(12)), (-0.05, -0.05));
+        assert!(!blocking.blocked());
+        blocking.reset();
+
+        blocking.update((Command::Back(120), Command::Back(120)), (-0.14, -0.05));
+        assert!(!blocking.blocked());
+        blocking.reset();
+
+        blocking.update((Command::Back(120), Command::Back(120)), (-0.14, -0.05));
+        assert!(!blocking.blocked());
+        blocking.reset();
+
+        // Backward / forward
+        blocking.update((Command::Back(120), Command::Back(120)), (0.14, 0.14));
+        assert!(blocking.blocked());
+        blocking.reset();
+
+        blocking.update((Command::Back(120), Command::Front(120)), (0.14, -0.14));
+        assert!(blocking.blocked());
+        blocking.reset();
     }
 }
